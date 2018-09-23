0cfb9dbf84a4d9f79d666aad5a427ca8c59a1c2f
ARDITYAYS <220499yogaputa@gmail.com>
2018-09-23 21:10:11 +0700

    first commit

---
+++ src/pbo310117119latihan10/PBO310117119Latihan10.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan10;
+
+/**
+ *
+ * @author  
+ * Nama      : Arditya Yogaputra S
+ * Kelas     : PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini berisi Logika 
+ */
+public class PBO310117119Latihan10 {
+
+    public static void main(String[] args) {
+       int umur =19;
+       boolean dewasa;
+       dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+               
+    }
+    
+}
